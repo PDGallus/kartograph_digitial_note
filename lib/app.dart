db860940e0e4edb21d8ac6932ae1ce10f4f0974f
Philip Hahn <phahn@brockhaus-ag.de>
2020-11-08 22:14:48 +0100

    add map with wastelands

--- lib/app.dart
+++ lib/app.dart
@@ -1,29 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:kartograph_digital_note/gameboard/game_board.dart';
+import 'package:kartograph_digital_note/gameboard/start_page.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Kartograph: Digitaler Spielplan'),
-            SizedBox(height: 50.0),
-            RaisedButton(
-              child: Text('Neues Spiel'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GameBoard())
-                );
-              },
-            )
-          ],
-        ),
+        child: StartPage(),
       ),
     );
   }
