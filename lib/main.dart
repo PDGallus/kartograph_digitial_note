eba931d3063fb8f254bde68201e5e8199c16d19f
Philip Hahn <phahn@brockhaus-ag.de>
2020-11-13 07:29:05 +0100

    set screen orientation to portrait up

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kartograph_digital_note/app.dart';
 import 'package:kartograph_digital_note/gameboard/gameboard_observer.dart';
 
 void main() {
   Bloc.observer = GameboardObserver();
   runApp(App());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
 }
